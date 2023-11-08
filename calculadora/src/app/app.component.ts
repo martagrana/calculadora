8f2e48e89df7c8c0b58ad779c2c5afe48bd8ddc1
Marta Grana <marta.grana.fdez@gmail.com>
2023-11-08 12:28:39 +0100

    cambios

--- calculadora/src/app/app.component.ts
+++ calculadora/src/app/app.component.ts
@@ -6,5 +6,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
   title = 'calculadora';
+
+  resultado: number = 0;
+
+  cifra1: number = 1;
+  cifra2: number = 2;
+  cifra3: number = 3;
+  cifra4: number = 4;
+  cifra5: number = 5;
+  cifra6: number = 6;
+  cifra7: number = 7;
+  cifra8: number = 8;
+  cifra9: number = 9;
+  cifra0: number = 0;
+
+
+  sumar() {
+    this.resultado = this.cifra2 + this.cifra3;
+
+  }
+  restar() {
+    this.resultado = this.cifra2 - this.cifra3;
+
+  }
+  multiplicar() {
+    this.resultado = this.cifra2 * this.cifra3;
+
+  }
+  dividir() {
+    this.resultado = this.cifra2 / this.cifra3;
+
+  }
 }
